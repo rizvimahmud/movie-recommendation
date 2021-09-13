2ff0ac82bf6038ac22239372a7272ef9508271e9
Rizvi Mahmud <rizvimahmud01@gmail.com>
2021-09-13 12:59:44 +0600

    configured tailwindcss

---
+++ data/data.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    title: "Interesteller",
+    likes: 210,
+  },
+  {
+    title: "Avenger's Endgame",
+    likes: 800,
+  },
+  {
+    title: "Tokyo Nights",
+    likes: 34,
+  },
+  {
+    title: "Requiem for a dream",
+    likes: 20,
+  },
+];
